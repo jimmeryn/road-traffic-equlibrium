61d12979718936c1887e5bdd611e07d9d649bd56
jimmeryn <jimmeryn@gmail.com>
2022-04-25 00:21:33 +0200

    Fixed new graph generator (OD matrix fix for real data)

--- src/shared/graph.py
+++ src/shared/graph.py
@@ -4,10 +4,7 @@ from typing import Dict
 
 from src.shared.link import Link
 from src.shared.node import Node
-
-
-def createLinkKey(from_node: int, to_node: int):
-    return f"{from_node}_{to_node}"
+from src.utils.link_utils import create_link_key
 
 
 class Graph:
@@ -16,19 +13,18 @@ class Graph:
     """
 
     def __init__(self, nodes, links, demands):
-        self.A = self.CreateLinksDict(links)
+        self.links = self.CreateLinksDict(links)
         self.nodes: Dict[int, Node] = self.CreateNodesDict(nodes)
-        self.n: int = nodes[-1][0]
+        self.n: int = int(nodes[-1][0])
 
-        self.SetFlows(demands)
-        self.SetLinksCosts()
         self.BuildTrees()
+        self.SetFlows(demands)
 
     def CreateLinksDict(self, links):
         return dict(
             map(lambda link: (
-                createLinkKey(int(link[0]), int(link[1])),
-                Link(link[0], link[1], link[4])
+                create_link_key(link[0], link[1]),
+                Link(*link)
             ), links)
         )
 
@@ -44,13 +40,10 @@ class Graph:
         for from_node, demands_array in enumerate(demands):
             for to_node, demand in enumerate(demands_array):
                 if demand != 0:
-                    link = self.A.get(createLinkKey(from_node + 1, to_node + 1))
-                    link.flow = demand
-                    link.cost = link.CalculateCost()
-
-    def SetLinksCosts(self):
-        for link in self.A.values():
-            link.cost = link.CalculateCost()
+                    link = self.nodes[to_node + 1].alpha_min
+                    while link is not None and link.src == from_node + 1:
+                        link.AddFlow(demand)
+                        link = self.nodes[link.src].alpha_min
 
     def BuildTrees(self):
         for i in range(1, self.n + 1):
@@ -63,7 +56,7 @@ class Graph:
         self.nodes[1].pi_min = 0
 
         for _ in range(1, self.n):
-            for link in self.A.values():
+            for link in self.links.values():
                 cij = link.cost
                 src = link.src
                 dest = link.dest
@@ -85,11 +78,32 @@ class Graph:
                     dest_node.alpha_max = link
 
     def GetLink(self, from_node, to_node):
-        return self.A.get(createLinkKey(from_node, to_node))
+        return self.links.get(create_link_key(from_node, to_node))
 
     def GetFlowArray(self):
         flow_array = []
-        for _key, link in self.A.items():
+        for link in self.links.values():
             flow_array.append(link.flow)
 
         return flow_array
+
+    def LogFlow(self) -> None:
+        for key, link in self.links.items():
+            print(f"{key}: {link.flow}")
+
+    def LogCosts(self) -> None:
+        for key, link in self.links.items():
+            print(f"{key}: {link.cost}")
+
+    def LogGraph(self) -> None:
+        for node in self.nodes.values():
+            print(node.index)
+            print(f"pi_max: {node.pi_max}")
+            print(f"pi_min: {node.pi_min}")
+            alpha_max = node.alpha_max
+            alpha_min = node.alpha_min
+            if alpha_max:
+                print(f"alpha_max: {node.alpha_max.src}_{node.alpha_max.dest}")
+            if alpha_min:
+                print(f"alpha_min: {node.alpha_min.src}_{node.alpha_min.dest}")
+            print("\n")
