047bad21665886dda4ee93aed704135c28e69122
jimmeryn <jimmeryn@gmail.com>
2022-05-09 00:55:31 +0200

    Changed Algorithm to extend ABC

--- src/algorithms/algorithm.py
+++ src/algorithms/algorithm.py
@@ -1,14 +1,23 @@
 """ Base Algorithm """
+from abc import ABC, abstractmethod
+
+from src.shared.graph import Graph
 
 
-class Algorithm():
+class Algorithm(ABC):
     """
     Base Algorithm abstract class
     All origin based alogrithms should extend this class
     """
+    @property
+    @abstractmethod
+    def graph(self) -> Graph:
+        pass
 
+    @abstractmethod
     def Iteration(self) -> None:
         raise NotImplementedError
 
+    @abstractmethod
     def GetMaxGap(self) -> None:
         raise NotImplementedError
