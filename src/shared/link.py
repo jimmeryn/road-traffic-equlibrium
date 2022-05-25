08b5f6ffad2f25dc63270870641a6b0f43077ae1
jimmeryn <jimmeryn@gmail.com>
2022-05-26 01:51:29 +0200

    Changed ints to floats

--- src/shared/link.py
+++ src/shared/link.py
@@ -12,13 +12,13 @@ class Link:
         init_node: int | float,
         term_node: int | float,
         capacity: float,
-        _length: int,
-        free_flow_time: int,
+        _length: float,
+        free_flow_time: float,
         b: float,
-        power: int,
-        _speed_limit: int,
-        _toll: int,
-        _link_type: int,
+        power: float,
+        _speed_limit: float,
+        _toll: float,
+        _link_type: float,
     ):
         self.src = int(init_node)
         self.dest = int(term_node)
@@ -26,23 +26,23 @@ class Link:
         self.b = b
         self.k = capacity
         self.p = power
-        self.flow = 0
+        self.flow = 0.0
         self.cost = free_flow_time
         self.cost_der = free_flow_time
 
-    def CalculateCost(self, flow: int | None = None):
+    def CalculateCost(self, flow: float | None = None):
         if flow is None:
             return self.CostFormula(self.flow)
 
         return self.CostFormula(flow)
 
-    def CalculateCostDerivative(self, flow: int | None = None):
+    def CalculateCostDerivative(self, flow: float | None = None):
         if flow is None:
             return self.CostDerivativeFormula(self.flow)
 
         return self.CostDerivativeFormula(flow)
 
-    def AddFlow(self, delta_flow):
+    def AddFlow(self, delta_flow: float):
         # Add flow to the link and update cost and cost derivative
         self.flow += delta_flow
         self.cost = self.CalculateCost()
@@ -51,12 +51,12 @@ class Link:
     def ResetFlow(self):
         self.flow = 0
         self.cost = self.fft
-        self.cost_der = self.fft
+        self.cost_der = 0
 
-    def CostFormula(self, x: int):
+    def CostFormula(self, x: float) -> float:
         # Use only when calculating new link cost or checking link cost for new flow
         return self.fft * (1 + self.b * math.pow(x / self.k, self.p))
 
-    def CostDerivativeFormula(self, x: int):
+    def CostDerivativeFormula(self, x: float) -> float:
         # Use only when calculating new link cost derivative or checking link cost derivative for new flow
-        return self.fft * x / self.k
+        return self.fft * self.b * self.p * math.pow(x / self.k, self.p - 1)
