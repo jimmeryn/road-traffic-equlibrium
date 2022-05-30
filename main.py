dc3c8df714546fa211a7aebc4c8d844a05e66e18
jimmeryn <jimmeryn@gmail.com>
2022-05-31 01:21:38 +0200

    Added env with test data

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 """ Main Program """
+import os
+
+from dotenv import load_dotenv
+
 from src.data.test import run_test
+from src.shared.consts import MAX_ERROR, MAX_ITERATION_COUNT
 from src.utils.user_input import UserInput
 
-MAX_ERROR = 1e-10
-MAX_ITERATION_COUNT = 3
+load_dotenv()
+
+ALGORITHM = os.getenv('ALGORITHM')
+EXAMPLE_NUMBER = os.getenv('EXAMPLE_NUMBER')
 
 
 def main():
     while True:
-        alg_index = UserInput.GetAlgorithm()
-        if alg_index == 0:
-            break
-        city_index = UserInput.GetCity()
-        if city_index == 0:
-            break
+        if not ALGORITHM:
+            alg_index = UserInput.GetAlgorithm()
+            if alg_index == 0:
+                break
+        else:
+            alg_index = int(ALGORITHM)
+        if not EXAMPLE_NUMBER:
+            city_index = UserInput.GetCity()
+            if city_index == 0:
+                break
+        else:
+            city_index = int(EXAMPLE_NUMBER)
 
-        run_test(city_index, alg_index, MAX_ERROR, MAX_ITERATION_COUNT)
-        if input("Press Enter to continue or 0 to exit.\n") == '0':
+        run_test(
+            city_index,
+            alg_index,
+            MAX_ERROR,
+            MAX_ITERATION_COUNT
+        )
+        if ALGORITHM or input("Press Enter to continue or 0 to exit.\n") == '0':
             break
 
 
